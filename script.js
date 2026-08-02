dbc4ae3b8f5fd19b6f55b49fcf5f7d20abc0f3ba
AlexC468 <chenalexg8@gmail.com>
2026-08-01 20:39:19 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Basic console log to test connection
-console.log("JavaScript is connected!");
-
-// Simple DOM manipulation example
-document.addEventListener("DOMContentLoaded", () => {
-    const heading = document.querySelector("h1");
-    // heading.textContent = "Hello from JavaScript!";
-});
