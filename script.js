92a279852c55931c5964a851d89fcbd43af1b248
AlexC468 <chenalexg8@gmail.com>
2026-08-01 20:37:37 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Basic console log to test connection
+console.log("JavaScript is connected!");
+
+// Simple DOM manipulation example
+document.addEventListener("DOMContentLoaded", () => {
+    const heading = document.querySelector("h1");
+    // heading.textContent = "Hello from JavaScript!";
+});
